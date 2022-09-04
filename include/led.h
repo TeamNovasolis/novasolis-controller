0b25874801f6b414a92e19c03112d9252712be9c
Orlando Guldimann <guldimann.orlando@gmail.com>
2022-09-04 14:58:45 +0200

    inital

---
+++ include/led.h
@@ -0,0 +1,7 @@
+void ledMain();
+void rainbow(int wait);
+void theaterChaseRainbow(int wait);
+void colorWipe(uint32_t color, int wait);
+void theaterChase(uint32_t color, int wait);
+void demo();
+void rain();
